d829d0be78e9f5dd12f47031933b0ca6bf0e853c
jarrodcolburn <jcourtlandcolburn@gmail.com>
2023-03-16 21:48:18 +0000

    converting event callbacks to Streams

--- lib/src/tabs/events.dart
+++ lib/src/tabs/events.dart
@@ -1,14 +1,22 @@
 @JS()
 part of "methods.dart";
 
-@JS()
-@anonymous
-class OnActivatedHandler {
-  external void addListener(void Function(ActiveInfo activeInfo) callback);
+@JS('chrome.tabs.onActivated')
+external void _onActivated(void Function(ActiveInfo activeInfo) callback);
+
+@JS('chrome.tabs.onRemoved')
+external void _onRemoved(void Function(int tabId, dynamic info) callback);
+
+Stream<ActiveInfo> get onActivated async* {
+  while (true) {
+    final completer = Completer<ActiveInfo>();
+    _onActivated((ActiveInfo activeInfo) {
+      completer.complete(activeInfo);
+    });
+    yield await completer.future;
+  }
 }
 
+Stream<Record<int,>> get onRemoved async* {}
+
 @JS()
-@anonymous
-class OnRemovedHandler {
-  external void addListener(void Function(int tabId, dynamic info) callback);
-}
