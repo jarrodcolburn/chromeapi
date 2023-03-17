56cfd9ab140d89937df62456710d8fc1aaeb1240
jarrodcolburn <jcourtlandcolburn@gmail.com>
2023-03-17 01:05:12 +0000

    extract helper moethods

--- lib/src/_helpers.dart
+++ lib/src/_helpers.dart
@@ -1,13 +1,4 @@
-library tabs;
-
-// import 'dart:js';
-// import 'dart:js_util';
 import 'dart:html_common';
-
-import 'package:js/js.dart';
-import 'package:js/js_util.dart';
-import '_legacy_tabs/types.dart';
-import '_legacy_tabs/methods.dart' as _tabs;
 
 extension _Dartify on Object {
   Future<List<T>> toFutureList<T>() async {
@@ -31,30 +22,3 @@ Future<List<T?>> _promiseToFutureList<T>(dynamic response) =>
       if (result is T) return <T>[result];
       throw Exception('Promise return type not expected');
     });
-
-Future<Tab?> duplicate(int tabId) =>
-    promiseToFuture<Tab?>(_tabs.duplicate(tabId));
-
-Future<List> executeScript(int tabId, InjectionDetails details) =>
-    _promiseToFutureList(_tabs.executeScript(tabId, details));
-
-Future<Tab?> get(int tabId) => promiseToFuture<Tab?>(_tabs.get(tabId));
-
-@Deprecated(
-    'getAllInWindow` is deprecated. Please use `tabs.query({windowId: windowId})`')
-Future getAllInWindow(int? windowId) => throw Exception(
-    'getAllInWindow` is deprecated. Please use `tabs.query({windowId: windowId})`');
-
-Future<Tab?> getCurrent() => promiseToFuture<Tab?>(_tabs.getCurrent());
-
-@Deprecated(
-    "`getSelected` is deprecated. Please use `tabs.query({active: true})`")
-Future getSelected(int? windowId) => throw Exception(
-    "`getSelected` is deprecated. Please use `tabs.query({active: true})`");
-
-Future<num> getZoom(int? tabId) => promiseToFuture<num>(_tabs.getZoom(tabId));
-
-// Future<List<Tab?>> query(QueryInfo queryInfo) => _promiseToFutureList<Tab>(_tabs.query(queryInfo));
-Future<List<Tab?>> query(QueryInfo queryInfo) =>
-    _tabs.query(queryInfo)?.toFutureList<Tab>() ??
-    (throw Exception('`tabs.query` did not return a promise'));
