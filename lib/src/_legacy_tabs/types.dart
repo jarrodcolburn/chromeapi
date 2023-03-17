401a35c372029b0d55172b25ab3f03d081063770
jarrodcolburn <jcourtlandcolburn@gmail.com>
2023-03-17 01:06:01 +0000

    comment out code to remove errors

--- lib/src/_legacy_tabs/types.dart
+++ lib/src/_legacy_tabs/types.dart
@@ -1,13 +1,40 @@
-@JS('chrome.tabs')
-library tabs;
-// File interop with javascript functionality exposed by chrome extensions.
+// @JS('chrome.tabs')
+// library tabs;
+// // File interop with javascript functionality exposed by chrome extensions.
 
-import 'package:js/js.dart' show JS, anonymous;
+// import 'package:js/js.dart' show JS, anonymous;
 
 
 
-/// JavaScript object(s) returned from `chrome.tabs.query`
-@JS()
-@anonymous
+// /// JavaScript object(s) returned from `chrome.tabs.query`
+// @JS()
+// @anonymous
 
-/// https://developer.chrome.com/docs/extensions/reference/tabs/#type-MutedInfo
+// /// https://developer.chrome.com/docs/extensions/reference/tabs/#type-MutedInfo
+
+// Future<Tab?> duplicate(int tabId) =>
+//     promiseToFuture<Tab?>(_tabs.duplicate(tabId));
+
+// Future<List> executeScript(int tabId, InjectionDetails details) =>
+//     _promiseToFutureList(_tabs.executeScript(tabId, details));
+
+// Future<Tab?> get(int tabId) => promiseToFuture<Tab?>(_tabs.get(tabId));
+
+// @Deprecated(
+//     'getAllInWindow` is deprecated. Please use `tabs.query({windowId: windowId})`')
+// Future getAllInWindow(int? windowId) => throw Exception(
+//     'getAllInWindow` is deprecated. Please use `tabs.query({windowId: windowId})`');
+
+// Future<Tab?> getCurrent() => promiseToFuture<Tab?>(_tabs.getCurrent());
+
+// @Deprecated(
+//     "`getSelected` is deprecated. Please use `tabs.query({active: true})`")
+// Future getSelected(int? windowId) => throw Exception(
+//     "`getSelected` is deprecated. Please use `tabs.query({active: true})`");
+
+// Future<num> getZoom(int? tabId) => promiseToFuture<num>(_tabs.getZoom(tabId));
+
+// // Future<List<Tab?>> query(QueryInfo queryInfo) => _promiseToFutureList<Tab>(_tabs.query(queryInfo));
+// Future<List<Tab?>> query(QueryInfo queryInfo) =>
+//     _tabs.query(queryInfo)?.toFutureList<Tab>() ??
+//     (throw Exception('`tabs.query` did not return a promise'));
