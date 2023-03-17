203cce811fb931787597657ec1cfaa43b0a78912
jarrodcolburn <jcourtlandcolburn@gmail.com>
2023-03-17 01:05:37 +0000

    make tab events streamable

--- lib/src/tabs/events.dart
+++ lib/src/tabs/events.dart
@@ -1,22 +1,48 @@
 @JS()
 part of "methods.dart";
 
-@JS('chrome.tabs.onActivated')
-external void _onActivated(void Function(ActiveInfo activeInfo) callback);
+// TODO: these functions need to be made reusable for other streams
 
-@JS('chrome.tabs.onRemoved')
-external void _onRemoved(void Function(int tabId, dynamic info) callback);
-
-Stream<ActiveInfo> get onActivated async* {
+Stream<ActivatedEvent> get onActivated async* {
   while (true) {
-    final completer = Completer<ActiveInfo>();
-    _onActivated((ActiveInfo activeInfo) {
-      completer.complete(activeInfo);
-    });
+    final completer = Completer<ActivatedEvent>();
+    void callback(ActiveInfo activeInfo) {
+      ActivatedEvent activatedEvent = (activeInfo:activeInfo,);
+      completer.complete(activatedEvent);
+    }
+    _onActivated(callback);
     yield await completer.future;
   }
 }
 
-Stream<Record<int,>> get onRemoved async* {}
+Stream<RemovedEvent> get onRemoved async* {
+  while (true) {
+    final completer = Completer<RemovedEvent>();
+    void callback(int tabId, RemoveInfo info) {
+      RemovedEvent removedEvent = (tabId:tabId,info:info);
+      completer.complete(removedEvent);
+    }
+    _onRemoved(callback);
+    yield await completer.future;
+  }
+}
+
+@JS('chrome.tabs.onActivated')
+external void _onActivated(void Function(ActiveInfo activeInfo) callback);
+
+@JS('chrome.tabs.onRemoved')
+external void _onRemoved(void Function(int tabId, RemoveInfo info) callback);
+
+typedef ActivatedEvent =({ActiveInfo activeInfo,}) ;
+
+typedef RemovedEvent =({int tabId , RemoveInfo info}) ;
 
 @JS()
+@anonymous
+class RemoveInfo {
+  external factory RemoveInfo({bool isWindowClosing, int windowId});
+  external bool get isWindowClosing;
+  external set isWindowClosing(bool newValue);
+  external int get windowId;
+  external set windowId(int newValue);
+}
