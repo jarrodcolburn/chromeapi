7d0d89e21a0fb2c9ddaa9a789db6f085c39376bb
jcourtlandcolburn <jcourtlandcolburn@gmail.com>
2023-03-19 00:02:27 -0500

    fix: dart doc objections

--- lib/src/tabs/properties.dart
+++ lib/src/tabs/properties.dart
@@ -3,6 +3,5 @@
 @JS('chrome.tabs')
 import 'package:js/js.dart';
 
-// ignore: non_constant_identifier_names
 external int get MAX_CAPTURE_VISIBLE_TAB_CALLS_PER_SECOND;
 external int get TAB_ID_NONE;
